7b111f33ba4ce60bd57de808a188337f0405c92a
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-04-07 20:38:25 -0300

    adicionando personagem principal e criando animações

--- JogoPlataforma/Assets/ScriptMegaman.cs
+++ JogoPlataforma/Assets/ScriptMegaman.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class ScriptMegaman : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rbd;
     public float vel;
+    private bool direita = true;
     // Start is called before the first frame update
     void Start()
     {
-        vel = 10;
+        vel = 6;
         rbd = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -19,6 +22,22 @@ public class ScriptMegaman : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         rbd.velocity = new Vector2(x * vel, rbd.velocity.y);
+
+        if (direita && x<0 || !direita && x > 0)
+        {
+            transform.Rotate(new Vector2(0,180));
+            direita = !direita;
+        }
+
+        if (x ==0)
+        {
+            anim.SetBool("movimento", false);
+        }else
+        {
+            anim.SetBool("movimento", true);
+        }
+
+
         
     }
 }
